720cae9f74d117b26bc45a906d89456a350084ea
Gyu Jeong Kim <gjkim@beyless.com>
2022-08-04 17:36:37 +0900

    Mobile

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,50 +1,99 @@
 <template>
 	<v-col class="ma-0 pa-0" cols="12">
-		<v-col class="ma-0 pa-0 mb-15" cols="12">
-			<v-col id="sub_introduce" class="ma-0 pa-0 pb-3" cols="12" align="left">
-				Overall
+		<template v-if="this.$store.state.isMobile == false">
+			<v-col class="ma-0 pa-0 mb-15" cols="12">
+				<v-col id="sub_introduce" class="ma-0 pa-0 pb-3" cols="12" align="left">
+					Overall
+				</v-col>
+				<v-col id="item_detail" class="ma-0 pa-0 pb-6" cols="12" align="left">
+					코드리뷰를 통해서 지식을 공유하고 같이 성장하는 것을 좋아합니다.<br>사용하기 쉽고 아름다운 유저 인터페이스를 구현하는데 흥미가 있습니다.<br>새로운 기술 스택을 배우고 적용하는데 거부감이 적습니다.<br>수시로 리팩토링을 하며 제3자가 보기 편한 클린코드를 지향합니다.<br>웹소켓을 이용해 실시간성이 보장된 서비스를 개발한 경험이 많습니다.
+				</v-col>
 			</v-col>
-			<v-col id="item_detail" class="ma-0 pa-0 pb-6" cols="12" align="left">
-				코드리뷰를 통해서 지식을 공유하고 같이 성장하는 것을 좋아합니다.<br>사용하기 쉽고 아름다운 유저 인터페이스를 구현하는데 흥미가 있습니다.<br>새로운 기술 스택을 배우고 적용하는데 거부감이 적습니다.<br>수시로 리팩토링을 하며 제3자가 보기 편한 클린코드를 지향합니다.<br>웹소켓을 이용해 실시간성이 보장된 서비스를 개발한 경험이 많습니다.
-			</v-col>
-		</v-col>
 
-		<v-col class="ma-0 pa-0 mb-15" cols="12">
-			<v-col id="sub_introduce" class="ma-0 pa-0 pb-3" cols="12" align="left">
-				Javascript
+			<v-col class="ma-0 pa-0 mb-15" cols="12">
+				<v-col id="sub_introduce" class="ma-0 pa-0 pb-3" cols="12" align="left">
+					Javascript
+				</v-col>
+				<v-col id="item_detail" class="ma-0 pa-0 pb-6" cols="12" align="left">
+					바닐라JS로 웹서비스 모듈을 구현한 경험이 있습니다.<br>ES6 문법, Node.js 개발환경에 익숙합니다.<br>Typescript를 도입하여 웹서비스를 개발한 경험이 있습니다.
+				</v-col>
 			</v-col>
-			<v-col id="item_detail" class="ma-0 pa-0 pb-6" cols="12" align="left">
-				바닐라JS로 웹서비스 모듈을 구현한 경험이 있습니다.<br>ES6 문법, Node.js 개발환경에 익숙합니다.<br>Typescript를 도입하여 웹서비스를 개발한 경험이 있습니다.
-			</v-col>
-		</v-col>
 
-		<v-col class="ma-0 pa-0 mb-15" cols="12">
-			<v-col id="sub_introduce" class="ma-0 pa-0 pb-3" cols="12" align="left">
-				Vue.js
+			<v-col class="ma-0 pa-0 mb-15" cols="12">
+				<v-col id="sub_introduce" class="ma-0 pa-0 pb-3" cols="12" align="left">
+					Vue.js
+				</v-col>
+				<v-col id="item_detail" class="ma-0 pa-0 pb-6" cols="12" align="left">
+					Vuetify, Vuex를 능숙하게 사용합니다.<br>라이프사이클을 이해하고 필요에 따라 컴포넌트, 함수들을 모듈화 할 수 있습니다.<br>Testing Library를 사용하여 테스트코드를 작성할 수 있습니다.<br>WMTS 지도를 이용한 실시간 웹서비스를 개발한 경험이 있습니다.
+				</v-col>
 			</v-col>
-			<v-col id="item_detail" class="ma-0 pa-0 pb-6" cols="12" align="left">
-				Vuetify, Vuex를 능숙하게 사용합니다.<br>라이프사이클을 이해하고 필요에 따라 컴포넌트, 함수들을 모듈화 할 수 있습니다.<br>Testing Library를 사용하여 테스트코드를 작성할 수 있습니다.<br>WMTS 지도를 이용한 실시간 웹서비스를 개발한 경험이 있습니다.
-			</v-col>
-		</v-col>
 
-		<v-col class="ma-0 pa-0 mb-15" cols="12">
-			<v-col id="sub_introduce" class="ma-0 pa-0 pb-3" cols="12" align="left">
-				Flutter, Dart
+			<v-col class="ma-0 pa-0 mb-15" cols="12">
+				<v-col id="sub_introduce" class="ma-0 pa-0 pb-3" cols="12" align="left">
+					Flutter, Dart
+				</v-col>
+				<v-col id="item_detail" class="ma-0 pa-0 pb-6" cols="12" align="left">
+					Dart 문법을 이해하고 능숙하게 사용할 수 있습니다.<br>앱 개발 및 배포(Android, iOS), 운영(앱 스토어), 유지보수 경험이 있습니다.<br>Android, iOS 빌드 과정에서의 트러블 슈팅 경험이 많습니다.<br>WMTS 지도를 이용한 앱을 개발한 경험이 있습니다.
+				</v-col>
 			</v-col>
-			<v-col id="item_detail" class="ma-0 pa-0 pb-6" cols="12" align="left">
-				Dart 문법을 이해하고 능숙하게 사용할 수 있습니다.<br>앱 개발 및 배포(Android, iOS), 운영(앱 스토어), 유지보수 경험이 있습니다.<br>Android, iOS 빌드 과정에서의 트러블 슈팅 경험이 많습니다.<br>WMTS 지도를 이용한 앱을 개발한 경험이 있습니다.
-			</v-col>
-		</v-col>
 
-		<v-col class="ma-0 pa-0 mb-15" cols="12">
-			<v-col id="sub_introduce" class="ma-0 pa-0 pb-3" cols="12" align="left">
-				DevOps, etc
+			<v-col class="ma-0 pa-0 mb-15" cols="12">
+				<v-col id="sub_introduce" class="ma-0 pa-0 pb-3" cols="12" align="left">
+					DevOps, etc
+				</v-col>
+				<v-col id="item_detail" class="ma-0 pa-0 pb-6" cols="12" align="left">
+					YONA, SLACK, NOTION 등을 사용한 협업에 익숙합니다.<br>Jenkins, Docker, AWS를 이용해 배포 자동화를 구축한 경험이 있습니다.<br>AWS만으로 웹서비스를 개발 및 운영한 경험이 있습니다.(EC2, S3, Amplify 등)<br>Github, Gitlab을 사용한 형상관리에 익숙합니다.<br>Zeplin을 사용해 디자이너와 소통합니다.<br>
+				</v-col>
 			</v-col>
-			<v-col id="item_detail" class="ma-0 pa-0 pb-6" cols="12" align="left">
-				YONA, SLACK, NOTION 등을 사용한 협업에 익숙합니다.<br>Jenkins, Docker, AWS를 이용해 배포 자동화를 구축한 경험이 있습니다.<br>AWS만으로 웹서비스를 개발 및 운영한 경험이 있습니다.(EC2, S3, Amplify 등)<br>Github, Gitlab을 사용한 형상관리에 익숙합니다.<br>Zeplin을 사용해 디자이너와 소통합니다.<br>
-			</v-col>
-		</v-col>
+		</template>
 
+		<!-- ############################### Mobile ############################### -->
+		<template v-else>
+			<v-col class="ma-0 pa-0 mb-15" cols="12">
+				<v-col id="sub_introduce_mobile" class="ma-0 pa-0 pb-3" cols="12" align="left">
+					Overall
+				</v-col>
+				<v-col id="item_detail_mobile" class="ma-0 pa-0 pb-6" cols="12" align="left">
+					코드리뷰를 통해서 지식을 공유하고 같이 성장하는 것을 좋아합니다.<br>사용하기 쉽고 아름다운 유저 인터페이스를 구현하는데 흥미가 있습니다.<br>새로운 기술 스택을 배우고 적용하는데 거부감이 적습니다.<br>수시로 리팩토링을 하며 제3자가 보기 편한 클린코드를 지향합니다.<br>웹소켓을 이용해 실시간성이 보장된 서비스를 개발한 경험이 많습니다.
+				</v-col>
+			</v-col>
+
+			<v-col class="ma-0 pa-0 mb-15" cols="12">
+				<v-col id="sub_introduce_mobile" class="ma-0 pa-0 pb-3" cols="12" align="left">
+					Javascript
+				</v-col>
+				<v-col id="item_detail_mobile" class="ma-0 pa-0 pb-6" cols="12" align="left">
+					바닐라JS로 웹서비스 모듈을 구현한 경험이 있습니다.<br>ES6 문법, Node.js 개발환경에 익숙합니다.<br>Typescript를 도입하여 웹서비스를 개발한 경험이 있습니다.
+				</v-col>
+			</v-col>
+
+			<v-col class="ma-0 pa-0 mb-15" cols="12">
+				<v-col id="sub_introduce_mobile" class="ma-0 pa-0 pb-3" cols="12" align="left">
+					Vue.js
+				</v-col>
+				<v-col id="item_detail_mobile" class="ma-0 pa-0 pb-6" cols="12" align="left">
+					Vuetify, Vuex를 능숙하게 사용합니다.<br>라이프사이클을 이해하고 필요에 따라 컴포넌트, 함수들을 모듈화 할 수 있습니다.<br>Testing Library를 사용하여 테스트코드를 작성할 수 있습니다.<br>WMTS 지도를 이용한 실시간 웹서비스를 개발한 경험이 있습니다.
+				</v-col>
+			</v-col>
+
+			<v-col class="ma-0 pa-0 mb-15" cols="12">
+				<v-col id="sub_introduce_mobile" class="ma-0 pa-0 pb-3" cols="12" align="left">
+					Flutter, Dart
+				</v-col>
+				<v-col id="item_detail_mobile" class="ma-0 pa-0 pb-6" cols="12" align="left">
+					Dart 문법을 이해하고 능숙하게 사용할 수 있습니다.<br>앱 개발 및 배포(Android, iOS), 운영(앱 스토어), 유지보수 경험이 있습니다.<br>Android, iOS 빌드 과정에서의 트러블 슈팅 경험이 많습니다.<br>WMTS 지도를 이용한 앱을 개발한 경험이 있습니다.
+				</v-col>
+			</v-col>
+
+			<v-col class="ma-0 pa-0 mb-15" cols="12">
+				<v-col id="sub_introduce_mobile" class="ma-0 pa-0 pb-3" cols="12" align="left">
+					DevOps, etc
+				</v-col>
+				<v-col id="item_detail_mobile" class="ma-0 pa-0 pb-6" cols="12" align="left">
+					YONA, SLACK, NOTION 등을 사용한 협업에 익숙합니다.<br>Jenkins, Docker, AWS를 이용해 배포 자동화를 구축한 경험이 있습니다.<br>AWS만으로 웹서비스를 개발 및 운영한 경험이 있습니다.(EC2, S3, Amplify 등)<br>Github, Gitlab을 사용한 형상관리에 익숙합니다.<br>Zeplin을 사용해 디자이너와 소통합니다.<br>
+				</v-col>
+			</v-col>
+		</template>
 	</v-col>
 </template>
 
